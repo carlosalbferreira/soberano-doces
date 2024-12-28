4fe4b2c02a7c9390ef12b92cbbb31c1dab558986
carlosalbferreira <carlinegaby@gmail.com>
2024-12-28 19:04:39 -0300

    Inserindo componentes header e cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CardsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'soberano-doces';
 }
+
